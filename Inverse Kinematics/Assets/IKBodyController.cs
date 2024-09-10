86c111125ba7c0d67cdaa7c8cf7acdac34fd8708
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-09-10 17:17:21 -0400

    Target Projection and PID func

--- Inverse Kinematics/Assets/IKBodyController.cs
+++ Inverse Kinematics/Assets/IKBodyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 
@@ -12,6 +13,13 @@ public class IKBodyController : MonoBehaviour
     Vector3 target;
     Vector3 orientation = Vector3.up;
     [SerializeField] float Height =  2f;
+
+    [Header("PID Settings")]
+    [SerializeField] float kp = 100f;
+    [SerializeField] float ki = 0.01f;
+    [SerializeField] float kd = 10f;
+    Vector3 cumulativeError = Vector3.zero;
+    Vector3 perviousError = Vector3.zero;
 
     void OnValidate() {
         Intialize();
@@ -34,8 +42,8 @@ public class IKBodyController : MonoBehaviour
         bodyPosition();
         //rb.position = targetVector;
         var mouseInput = Input.mousePositionDelta.x;
-        Cursor.lockState = CursorLockMode.Locked;
-        transform.Rotate(new Vector3(0, mouseInput*0.2f, 0));
+        //Cursor.lockState = CursorLockMode.Locked;
+        //transform.Rotate(new Vector3(0, mouseInput*0.2f, 0));
         if (Input.GetKey(KeyCode.E))
         {
             transform.Rotate(new Vector3(0, 1, 0));
@@ -52,13 +60,27 @@ public class IKBodyController : MonoBehaviour
         Vector3 force = target - transform.position;
         force = new Vector3(force.x, Mathf.Clamp(force.y, -1f, float.PositiveInfinity), force.z);
         if(force.magnitude < 0.5f) force = force.normalized * Mathf.Sqrt(force.magnitude);
-        rb.AddForce(force * Time.deltaTime*200);
+        rb.AddForce(force*200*Time.deltaTime);
         //rb.transform.position = target;
         //rb.MovePosition(target);
         orientation = Vector3.SmoothDamp(orientation, ik.GetOrientation(), ref velocity, 1f);
         //transform.up = orientation;
         Quaternion targetRotation = Quaternion.FromToRotation(transform.up, orientation) * transform.rotation;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360f * Time.deltaTime);
+    }
+
+    Vector3 PID(Vector3 position, Vector3 target) {
+        Vector3 force = Vector3.zero;
+        Vector3 error = target-position;
+        Vector3 deltaError = error-perviousError;
+        cumulativeError += error;
+        force += error*kp;
+        force += error*ki;
+        force += deltaError*kd;
+        
+        perviousError = error;
+
+        return force;
     }
 
     private void Move()
@@ -84,7 +106,7 @@ public class IKBodyController : MonoBehaviour
         Vector3 averagePos = ik.AveragePosition();
         //float basetarget = averagePos.y+Height;
         Vector3 basetarget = new Vector3(transform.position.x,averagePos.y, transform.position.z);
-        target = averagePos + transform.up.normalized * Height;
+        target = Vector3.Project(averagePos, transform.up) + Vector3.ProjectOnPlane(transform.position, transform.up) + transform.up.normalized * Height;
     }
     void OnDrawGizmos() {
         Gizmos.DrawSphere(target, 0.5f);
